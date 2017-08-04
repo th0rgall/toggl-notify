cefee366a3f2040fe794e5cf3fc3c6d638d76a71
Thor Galle <thor.galle@student.kuleuven.be>
2017-08-04 22:13:50 +0200

    Use browserify, trying jquery & rxjs

--- toggl-notify.js
+++ toggl-notify.js
@@ -1,17 +1,34 @@
+var Rx = require('rxjs/Rx');
+var $ = require("jquery");
 
 $(document).ready(
   function() {
 
     setTimeout(getData, 3500);
 
+    function createControls() {
+      return $('<div>', {
+        class: "DateTimeDurationPopdown__header",
+        id: "notify-controls"
+      });
+    }
+
     function getData() {
+
+      // insert controls
+      $(".Timer__timer .DateTimeDurationPopdown__popdown > div")
+        .append(createControls());
+
+      // log time
       var timer_duration = $(".Timer__duration");
       if (timer_duration.length > 0) {
-        timer_duration[0].addEventListener("DOMNodeRemoved", function(event) {
-          console.log("helala" + event.target);}
-        );
+
+        var timeObs = Rx.Observable.fromEvent(timer_duration[0], "DOMNodeRemoved")
+          .bufferCount(2,2);
+
+        timeObs.subscribe(() => console.log("helalaaaa!"));
       }
     }
 
   }
-)
+);
